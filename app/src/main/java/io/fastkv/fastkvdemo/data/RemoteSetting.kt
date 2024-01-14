ec30b26e7d9b562be3e299ecbbe94de1ae34dc9a
BillyWei <across_horizon@qq.com>
2024-01-14 12:27:51 +0800

    Optimize use cases

--- app/src/main/java/io/fastkv/fastkvdemo/data/RemoteSetting.kt
+++ app/src/main/java/io/fastkv/fastkvdemo/data/RemoteSetting.kt
@@ -9,7 +9,13 @@ import io.fastkv.fastkvdemo.fastkv.kvbase.UserKV
  * 所以，uid固定为0（其他的uid用不到的数值也可以）。
  */
 object RemoteSetting : UserKV("remote_setting", 0L) {
-    val showGuide by boolean("show_guide")
+    /**
+     * 某项功能的AB测试分组
+     */
+    val fun1ABTestGroup by int("fun1_ab_test_group")
 
+    /**
+     * 服务端下发的配置项
+     */
     val setting by combineKey("setting")
 }
